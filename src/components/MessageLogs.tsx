27b3920b73e72b22958da2e3213b1ae3ce9528fd
supertiger1234 <saawalkhn@googlemail.com>
2020-11-06 19:50:43 +0000

    optimised

--- src/components/MessageLogs.tsx
+++ src/components/MessageLogs.tsx
@@ -6,28 +6,28 @@ import {FlatList, View} from 'react-native';
 import {storeContext} from '../stores/RootStore';
 import MessageTemplate from './MessageTemplate';
 
+const renderRow = ({item}: {item: any}) => {
+  return <MessageTemplate message={item[0]} grouped={item[1]} />;
+};
+const keyExtractor = (item: any) => item[0].tempID || item[0].messageID;
+
 export default observer(() => {
   const flatList = useRef<FlatList>(null);
   const {stateStore, messageStore} = React.useContext(storeContext);
 
   const channelID = stateStore.selectedChannelID;
-  const messages = messageStore.groupedChannelMessages(channelID || '');
-  // const messages = messageStore.messages[channelID || ''];
+  const messages = messageStore
+    .groupedChannelMessages(channelID || '')
+    ?.slice()
+    .reverse();
 
-  const renderRow = ({item}: {item: any}) => {
-    return <MessageTemplate message={item[0]} grouped={item[1]} />;
-  };
-  const keyExtractor = (item: any) => item[0].tempID || item[0].messageID;
-
+  if (!messages.length) return <View />;
   return (
     <View style={{flex: 1}}>
       <FlatList
         ref={flatList}
         data={messages}
-        onContentSizeChange={() =>
-          flatList.current?.scrollToEnd({animated: true})
-        }
-        onLayout={() => flatList.current?.scrollToEnd({animated: true})}
+        inverted
         renderItem={renderRow}
         keyExtractor={keyExtractor}
       />
